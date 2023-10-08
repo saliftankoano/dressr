e68f89a944631b769ac150a0340f5c11ae131d01
rafi-d-z <rafidizia@gmail.com>
2023-10-07 20:37:10 -0400

    added user input; makes api request for any zipcode inputted by user

--- src/Weather.js
+++ src/Weather.js
@@ -1,40 +1,60 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import axios from 'axios';
 
-function Weather(){
-    const [temperature, setTemperature] = useState(null);
-    const [windSpeed, setWindSpeed] = useState(null);
-    const [loading, setLoading] = useState(true);
+function Weather() {
+    const [weatherData, setWeatherData] = useState(null);
+    const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
+    const [zipcode, setZipcode] = useState("");
 
-    useEffect(()=>{
-        const key = '3a9ff8978a1b48868a224538232909';
-        const zipcode = '11369';
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-        axios.get(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${zipcode}&aqi=no`)
-            .then((response) => {
-            const temperature = response.data.current.temp_f;
-            const windSpeed = response.data.current.wind_mph;
+    const fetchData = async () => {
+        try {
+            const key = '3a9ff8978a1b48868a224538232909'
+            setLoading(true);
+            setError(null);
 
-            setTemperature(temperature);
-            setWindSpeed(windSpeed);
+            const response = await axios.get(`http://api.weatherapi.com/v1/current.json?key=${key}&q=${zipcode}&aqi=no`);
+            setWeatherData(response.data.current);
             setLoading(false);
-        })
-            .catch((err) => {
+        } catch (err) {
             setError(err);
             setLoading(false);
-        });
-    },[]);
+        }
+    };
+
+    const handleChange = (e) => {
+        setZipcode(e.target.value);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        fetchData();
+    };
 
     return (
         <div>
-          <h1>Zipcode: 11369</h1>
-          {loading && <p>Loading...</p>}
-          {error && <p>Error: {error.message}</p>}
-          {temperature && <p>Temperature: {temperature}&deg;F</p>}
-          {windSpeed && <p>Wind Speed: {windSpeed} mph</p>}
+            <h1>Weather Information</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Zipcode:
+                    <input name="zipcode" value={zipcode} type="text" placeholder="Enter Zip Code" onChange={handleChange} />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {weatherData && (
+                <div>
+                    <p>Temperature: {weatherData.temp_f}&deg;F</p>
+                    <p>Wind Speed: {weatherData.wind_mph} mph</p>
+                </div>
+            )}
         </div>
-      );
+    );
 }
 
 export default Weather;
