4a467ee1dbf26090b28baab4dafc05525e932e96
rafi-d-z <rafidizia@gmail.com>
2023-11-02 21:42:06 -0400

    added outfits.jsx, updated package.json, added rafidtempstyle.css (temporary style sheet),

--- client/src/components/Weather.jsx
+++ client/src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
+import './RafidTempStyle.css'
 
 function Weather() {
     const [weatherData, setWeatherData] = useState(null);
